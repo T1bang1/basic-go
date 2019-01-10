14a2ff974fe8536efcd4c4d408da4d2a3fc3911d
t1duvan <caiceduvan@gmail.com>
2019-01-10 13:32:16 -0500

    h

---
+++ conversion.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+type hotdog int
+
+var x hotdog
+var y int
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("el tipo de la variable “x” es: %T\n", x)
+
+	x := 42
+	fmt.Println(x)
+
+	y := int(x)
+	fmt.Println(y)
+fmt.Printf("el tipo de la variable “y” es: %T\n", y)
+}
